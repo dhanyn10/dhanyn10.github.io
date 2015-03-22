e9293eef8348c8368021788a2c4586603fe99368
dhanyn10 <dhanyn@outlook.co.id>
2015-03-22 22:53:20 +0700

    update

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+	<head>
+		<link href="css/index-style.css" rel="stylesheet"></link>
+		<link href="css/font.css" rel="stylesheet"></link>
+		<title>Beranda</title>
+	</head>
+	<body>
+		<div class="outer-wrapper">
+			<div class="header-wrapper">
+				<h1>Dhany Nurdiansyah</h1>
+			</div>
+			<nav class="navigasi">
+				<ul id="navigasi">
+					<li class="active"><a href="index.php">Beranda</a></li>
+					<li><a href="profil.php">Profil</a></li>
+					<li><a href="kontak.php">Kontak</a></li>
+					<li><a href="http://dhanynurdiansyah.blogspot.com"target="_blank>Blog Saya</a></li>
+				</ul>
+			</nav>
+			<div class="main-wrapper">
+				<div id="foto">
+					<img src="foto/ice-skating.jpg"></img>
+				</div>
+			</div>
+			<div class="footer-wrapper">Copyright &copy; <?php echo date("Y"); ?> Dhany Nurdiansyah
+			</div>
+		</div>
+	</body>
+</html>
---
+++ profil.php
@@ -0,0 +1,53 @@
+<html>
+	<head>
+		<link href="css/profil.css" rel="stylesheet"></link>
+		<link href="css/font.css" rel="stylesheet"></link>
+		<title>Profil</title>
+	</head>
+	<body>
+		<div class="header-wrapper">
+			<h1>Dhany Nurdiansyah</h1>
+		</div>
+		<nav class="navigasi">
+			<ul id="navigasi">
+				<li><a href="index.php">Beranda</a></li>
+				<li class="active"><a href="profil.html">Profil</a></li>
+				<li><a href="kontak.php">Kontak</a></li>
+				<li><a href="http://dhanynurdiansyah.blogspot.com"target="_blank">Blog Saya</a></li>
+			</ul>
+		</nav>
+		<div class="main-wrapper">
+		<table>
+		<tr>
+			<th id="top"><p>Basic Info</p></th>
+			<td>
+				<table>
+					<tr>
+						<td>Nama</td>
+						<td>:</td>
+						<td>Dhany Nurdiansyah</td>
+					</tr>
+					<tr>
+						<td>Pekerjaan</td>
+						<td>:</td>
+						<td>Mahasiswa | Blogger | Web Master</td>
+					</tr>
+					<tr>
+						<td>Kontak</td>
+						<td>:</td>
+						<td>085-850-551-415</td>
+					</tr>
+					<tr>
+						<td>Alamat</td>
+						<td>:</td>
+						<td>Ds. Pojok, Kec. Ngantru, Kab. Tulungagung, Jawa Timur</td>
+					</tr>
+				</table>
+			</td>
+		</tr>
+		</table>
+		</div>
+		<div class="footer-wrapper">Copyright &copy; <?php echo date("Y"); ?> Dhany Nurdiansyah
+		</div>
+	</body>
+</html>
