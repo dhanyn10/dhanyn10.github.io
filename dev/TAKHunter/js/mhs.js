a474dbc63ab75c6ab03b4ff23b7e9d915b117004
Dhany Nurdiansyah <dhanyn@outlook.co.id>
2015-11-28 16:10:09 +0700

    update TAKHunter

--- dev/TAKHunter/js/mhs.js
+++ dev/TAKHunter/js/mhs.js
@@ -12,16 +12,12 @@ $('document').ready(function(){
             $('table')[0].rows[a].cells[1].innerHTML = "<input onkeyup='gantinilai()' class='atur-profil' type='text' placeholder='"+p[a]+"'/>";
         }
     });
+function gantinilai(){
+    var n = $('table')[0].getElementsByTagName('tr').length;
+    var profil = Array(n);
+    document.getElementById('perbarui').value = "";
     for(a = 0; a < n; a++){
-        document.getElementsByClassName('atur-profil')[a]
+        profil[a] = document.getElementsByClassName('atur-profil')[a].value;
+        document.getElementById('perbarui').value += profil[a];
     }
-//        var up = Array(n);
-//        var c = "";
-//        for(a = 0; a<n; a++){
-//            up[a] = $('table')[0].rows[a].cells[1].getElementsByTagName('input')[0].val();
-//        }
-//        for(a = 0; a<n; a++){
-//            c += up[a];
-//        }
-//        $('#perbarui').val() = c;
-});
+}
