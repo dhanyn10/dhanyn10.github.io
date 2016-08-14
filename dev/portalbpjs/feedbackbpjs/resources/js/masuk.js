da6c24ae30288790449126bd2e267a35810c2bc8
Dhany Nurdiansyah <dhanyn@outlook.co.id>
2016-08-14 21:31:54 +0700

    data Struktur Organisasi BPJS Kesehatan

--- dev/portalbpjs/feedbackbpjs/resources/js/masuk.js
+++
@@ -1,8 +0,0 @@
-$(".admin").click(function(){
-    $(".admin-form").css("display","block");
-    $(".user-form").css("display","none");
-});
-$(".user").click(function(){
-    $(".user-form").css("display","block");
-    $(".admin-form").css("display","none");
-});
