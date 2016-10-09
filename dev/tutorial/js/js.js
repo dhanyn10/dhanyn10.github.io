0ffcde2cd66dc896f343037bf0a8289253846476
Dhany Nurdiansyah <dhanyn@outlook.co.id>
2016-10-09 20:10:19 +0700

    ubah calculator.html menjadi index.html, perbaikan mobile view

--- dev/tutorial/js/js.js
+++ dev/tutorial/js/js.js
@@ -4,3 +4,16 @@ function gantiTag(kode)
     kode = kode.replace(new RegExp(">","g"), "]");
     return kode;
 }
+$(document).ready(function(){
+    //disable bracket input
+    $(".asalkode").each(function(){
+        $(".asalkode").keydown(function(k){
+            //219 keycode for "["
+            //221 keycode for "]"
+            if(k.which == 219 || k.which == 221 || k.which == 32)
+                {
+                    return false;
+                }
+        });
+    }); 
+});
--- dev/tutorial/tutorial-html/lesson1.js
+++ dev/tutorial/tutorial-html/lesson1.js
@@ -1,17 +1,4 @@
 $(document).ready(function(){
-    
-    //disable bracket input
-    $(".asalkode").each(function(){
-        $(".asalkode").keydown(function(k){
-            //219 keycode for "["
-            //221 keycode for "]"
-            if(k.which == 219 || k.which == 221 || k.which == 32)
-                {
-                    return false;
-                }
-        });
-    });
-    
     /* ===================================
      * AUTO RUN
      * ===================================
