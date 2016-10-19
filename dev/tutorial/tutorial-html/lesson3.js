097aa5730c96ccfad132040efbf6c5c9536934cb
Dhany Nurdiansyah <dhanyn@outlook.co.id>
2016-10-19 09:15:29 +0700

    update lesson 3

--- dev/tutorial/tutorial-html/lesson3.js
+++ dev/tutorial/tutorial-html/lesson3.js
@@ -8,4 +8,13 @@ $(document).ready(function(){
         c_teks = $(".asalkode").eq(0).val();
         $(".hasilkode").eq(0).html(c_teks);
     }
+    
+    merah_kuning_hijau();
+    (".asalkode").eq(1).keyup(merah_kuning_hijau);
+
+    function merah_kuning_hijau()
+    {
+        c_teks = $(".asalkode").eq(1).val();
+        $(".hasilkode").eq(1).html(c_teks);
+    }
 });
